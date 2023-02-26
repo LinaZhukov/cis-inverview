9a159c17f2d577be54925aab1f529d2694169156
lina-lp <lina@lilypad.co>
2023-02-26 18:50:19 -0500

    update names

--- src/main/java/com/example/demo/AnimalResource.java
+++ src/main/java/com/example/demo/AnimalResource.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-world")
-public class HelloResource {
+@Path("/animals")
+public class AnimalResource {
+    @GET
+    public Animal[] getAnimals(){
+        Animal[] animals = new Animal[3];
+        return animals;
+    }
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
---
+++ src/main/java/com/example/demo/db.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class db {
+    public Object write(Object data){
+        return data;
+    }
+
+    public Animal[] read(Object query){
+        return new Animal[1];
+    }
+}
